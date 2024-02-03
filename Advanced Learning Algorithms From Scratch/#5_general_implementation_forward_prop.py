52ec839e5e95dc9d06ad29af5d09a02c760d68a5
ammarlodhi255 <ammarlodhi68@gmail.com>
2024-02-03 15:46:15 +0500

    add general implementation of forward prop

---
+++ Advanced Learning Algorithms From Scratch/#5_general_implementation_forward_prop.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def g(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def dense(a_in, W, b):
+    units = W.shape[1]
+    a_n = np.zeros((W.shape[1], ))
+
+    for i in range(units):
+        w = W[:, i]
+        out = np.dot(a_in, w) + b[i]
+        a_n[i] = g(out)
+
+    return a_n
+
+
+if __name__ == '__main__':
+    W = np.array([
+        [1, -3, 5],
+        [2, 4, -6]
+    ])
+
+    b = np.array([-1, 1, 2])
+
+    a_in = np.array([-2, 4])
+
+
+    print(dense(a_in, W, b))
